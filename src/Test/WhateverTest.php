fd46d7ed1db6dcbe8cbc20277635ad09c7114c4a
Miguel Manzano <mmanzano@gmail.com>
2016-02-11 10:29:22 +0100

    Initial Commit

---
+++ src/Test/WhateverTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Code\Test;
+
+use Code\Whatever;
+
+class WhateverTest extends \PHPUnit_Framework_TestCase
+{
+    /** @test */
+    public function whatever_test()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ src/WhateverExecution.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Code;
+
+require_once '../vendor/autoload.php';
+
+$input = file_get_contents("WhateverInput");
+
+$Request = new WhateverInput($input);
